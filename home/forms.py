ba41cf0689cd825ceca86ab0e2adb059c223755f
Luiz Martins <luizmartins.uemg@gmail.com>
2024-01-13 20:27:37 -0300

    Create page of contact for mockbank

---
+++ home/forms.py
@@ -0,0 +1,55 @@
+from django import forms 
+from django.core.exceptions import ValidationError
+import re 
+
+from home.models import ContactHome
+
+EMAIL_REGEX = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactHome 
+        fields =  ("name", "email", "message")
+        labels = {
+            "name": "Nome", 
+            "email": "Email", 
+            "message": "Mensagem", 
+        }
+
+        widgets= {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control', 
+                'id': 'txtNome', 
+                'placeholder': " ", 
+                'autofocus': True,
+            }), 
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'id': 'txtEmail', 
+                'placeholder': " ", 
+                'autofocus': True, 
+            }), 
+            'message': forms.Textarea(attrs={
+                'class': 'form-control', 
+                'id': 'txtmensagem', 
+                'cols': "30", 
+                'rows': '10', 
+                'placeholder': ' ',
+                'style':  "height:200px", 
+            })
+
+        }
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if email and not re.match(EMAIL_REGEX, email):
+            self.add_error(
+                'name', 
+                ValidationError(
+                    'Email inválido', 
+                    code='invalid'
+                )
+            )
+        
+        return email 
--- home/views.py
+++ home/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 
+from home.forms import ContactForm
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def contact_us(request):
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            name = request.POST['name']
+            
+            form.save()
+
+            return render(request, 'contact_confirm.html', {'name': name})
+        
+        return render(request, 'contact.html', {'form': form})
+    
+    form = ContactForm()
+
+    context = {
+        "form": form, 
+    }
+
+    return render(request, 'contact.html', context)
